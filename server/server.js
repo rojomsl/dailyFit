5e943615e5e23c6e26021489d50315d411195892
Missael <missael@mappen.mx>
2025-06-05 18:22:06 -0600

    :hammer:

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, '../client')));
 
 
 app.get('/dailyFit', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/index.html'));
+  res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 // Insertar progreso
